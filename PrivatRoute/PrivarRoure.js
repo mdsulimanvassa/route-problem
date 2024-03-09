964dfa78b53a73665fa0f87bccb71f4e45af826b
MD Suliman P Hero <150431927+mdsulimanvassa@users.noreply.github.com>
2024-03-09 13:02:39 +0600

    Update PrivarRoure.js

--- PrivatRoute/PrivarRoure.js
+++ PrivatRoute/PrivarRoure.js
@@ -1,15 +1,10 @@
 import React, { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate} from 'react-router-dom';
 import { UserContex } from '../../App';
 
-const PrivatRoute = ({children}) => {
-    const [loggedInUser, setLoggedInUser] = useContext(UserContex);
-
-    if(!loggedInUser) {
-        return loggedInUser ? children : <Navigate replace to="/login" />
-  }
-  else{
-    return children;
-  }
- }
-export default PrivatRoute;
+function PrivateRoutes({children}) {
+    const [loggInUser] = useContext(UserContex);
+          return ( loggInUser ? children: <Navigate to = '/login' />)
+        
+}
+export default PrivateRoutes;
